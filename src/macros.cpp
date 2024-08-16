2aa34c37bd9f46d806158fbd97b17bc6cb7b1517
linem davton <linem.davton@gmail.com>
2024-08-16 09:48:41 +0200

    added macros to cmake

--- src/macros.cpp
+++ src/macros.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 
 #define max(a, b) a > b ? a : b
+#define mult(a, b) a *b
 
 auto main() -> int {
-  int a = 5, b = 10;
+  int a = 12, b = 10;
   auto c = max(a, --b);
-  std::cout << c << std::endl;
+  std::cout << c << " " << b
+            << "\n"; // --b is evaluated potentially twice and atleast once
+
+  int d = 2, e = 3;
+  c = mult(d + 1, e); // 2 + 1 * 3, not 3 * 3
+  std::cout << c << "\n";
   return 0;
 }
