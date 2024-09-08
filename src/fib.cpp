d44c7ebc5948615f29edc43b84438cd2cad35189
linem davton <linem.davton@gmail.com>
2024-09-08 18:56:20 +0200

    fib

---
+++ src/fib.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int fib(int n) {
+  if (n <= 1) {
+    return n;
+  }
+  return fib(n - 1) + fib(n - 2);
+}
+
+int main() {
+  int n = 10;
+  std::cout << "fib(" << n << ") = " << fib(n) << "\n";
+  return 0;
+}
