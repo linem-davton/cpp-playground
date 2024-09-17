f9e30ba2c9a95a30ccaa98ad4673732c9155a9d6
linem davton <linem.davton@gmail.com>
2024-09-17 16:46:19 +0200

    tests: matrices test

--- tests/test_matrix.cpp
+++ tests/test_matrix.cpp
@@ -1,22 +1,25 @@
 #include "matrices.h"
 #include <gtest/gtest.h>
 
-TEST(MatrixTest, MatrixMultiply) {
-  const int N1 = 1000;
-  const int M1 = 1000;
-  const int N2 = 1000;
-  const int M2 = 1000;
+TEST(MatrixTest, MatrixGen) {
+  const int N1 = 10;
+  const int M1 = 10;
   const int min_val = 1;
   const int max_val = 99;
-
   const mat::MatrixInt matrix1 =
       mat::generateRandomMatrix({N1, M1}, {min_val, max_val});
-  const mat::MatrixInt matrix2 =
-      mat::generateRandomMatrix({N2, M2}, {min_val, max_val});
 
-  const auto result = mat::matrixMultiply(matrix1, matrix2, {N1, M1}, {N2, M2});
-  if (!result) {
-    FAIL();
-  }
-  SUCCEED();
+  EXPECT_EQ(matrix1.size(), N1);
+  EXPECT_EQ(matrix1[0].size(), M1);
+}
+TEST(MatrixTest, MatrixMul) {
+  std::vector<std::vector<int>> A = {{1, 2}, {3, 4}};
+
+  std::vector<std::vector<int>> B = {{5, 6}, {7, 8}};
+
+  // Expected result of A * B
+  std::vector<std::vector<int>> expected = {{19, 22}, {43, 50}};
+
+  const auto result = mat::matrixMultiply(A, B, {2, 2}, {2, 2});
+  EXPECT_EQ(result.value(), expected);
 }
