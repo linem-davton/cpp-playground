4c7331d9bfdd8665dd14aa7e7848e59e6735a694
linem davton <linem.davton@gmail.com>
2024-09-19 11:38:13 +0200

    build_all config script

---
+++ build_all.sh
@@ -0,0 +1,13 @@
+cmake --preset=debug
+cmake --build --preset=debug
+
+cmake --preset=release
+cmake --build --preset=release
+
+cmake --preset=debug-sanitize
+cmake --build --preset=debug-sanitize
+
+cmake --preset=release-lto
+cmake --build --preset=release-lto
+
+
