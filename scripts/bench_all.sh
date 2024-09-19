a1567a1263844cdf592cd30f555bef325012ab17
linem davton <linem.davton@gmail.com>
2024-09-19 13:24:12 +0200

    cleanup

--- scripts/bench_all.sh
+++ scripts/bench_all.sh
@@ -9,6 +9,6 @@ build_presets=("debug" "debug-sanitize" "release" "release-lto" "release-sanitiz
 for preset in "${build_presets[@]}"; do
   echo -e "\n\n\033[1;32mBechmarking for preset: $preset \033[0m"
   for benchmark in "${benchmarks_executable[@]}"; do
-    ./build/$preset/bin/$benchmarks_executable
+   sudo perf stat ./build/$preset/bin/$benchmarks_executable
   done
 done
