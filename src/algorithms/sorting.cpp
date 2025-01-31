ea36a65930c77ffaff08bf13dde21149c0877651
linem davton <linem.davton@gmail.com>
2025-01-31 07:03:21 +0100

    Sorting: Bubble sort with early stopping

--- src/algorithms/sorting.cpp
+++ src/algorithms/sorting.cpp
@@ -18,10 +18,20 @@ template <typename T>
 void bubble_sort(std::vector<T>& v) {
     int n = v.size();
     for (int i = 0; i < n; i++) {
+        // One pass of internal loop should swap atleast one element, if not then array is already sorted
+        bool swapped = false;
         for (int j = 0; j < n - 1; j++) {
             if (v[j] > v[j + 1]) {
                 swap(v[j], v[j + 1]);
+                swapped = true;
             }
+            std::cout << "Array after iteration " << i << " and " << j << '\n';
+            printVec<T>(v);
+        }
+
+        if (!swapped) {
+            std::cout << "Array is already sorted after iteration: " << i << " instead of: " << n - 1 << "\n";
+            break;
         }
     }
 }
@@ -35,15 +45,15 @@ auto main(int argc, char* argv[]) -> int {
         std::cin >> size;
     }
     std::vector<int> vec = random_vector<int>(size);
-    std::vector<double> vec1 = random_vector<double>(size);
+    // std::vector<double> vec1 = random_vector<double>(size);
 
     printVec<int>(vec);
-    printVec<double>(vec1);
+    // printVec<double>(vec1);
 
     bubble_sort(vec);
-    bubble_sort(vec1);
+    // bubble_sort(vec1);
 
     printVec<int>(vec);
-    printVec<double>(vec1);
+    // printVec<double>(vec1);
     return 0;
 }
