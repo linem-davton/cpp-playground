86252e48f96af96696828d250b24a0b6a6789120
linem davton <linem.davton@gmail.com>
2025-02-06 12:54:03 +0100

    searching: header file

---
+++ include/search_array.h
@@ -0,0 +1,59 @@
+#include <algorithm>
+#include <optional>
+#include <vector>
+/* Classic Binary Search
+ * Time Complexity - O(log n), Space Complexity - O(1)
+ * Note: Array should be sorted
+ * Not guranteed to return the first or last matching element
+ *
+ */
+
+template <typename T>
+auto binary_search(const std::vector<T>& vec, T key) -> std::optional<int> {
+    int size = vec.size();
+    int low = 0;
+    int high = size - 1;
+    while (low <= high) {
+        int mid = (high + low) / 2;
+        if (vec[mid] == key) {
+            return mid;
+        }
+        if (vec[mid] < key) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+    return std::nullopt;
+}
+/* Cache Efficient Binary Search
+ * Note: Array should be sorted
+ * Not guranteed to return the first or the last matching element
+ */
+template <typename T>
+auto binary_jump_search(const std::vector<T>& vec, T key) -> std::optional<int> {
+    int size = vec.size();
+
+    int index = 0;
+    for (int jump = size / 2; jump >= 1; jump /= 2) {
+        while (index + jump < size && vec[index + jump] <= key) {
+            index += jump;
+        }
+    }
+    if (vec[index] == key) {
+        return index;
+    }
+    return std::nullopt;
+}
+/* std::find() - returns iterator to first matching element, else vec.end()
+ * Array doesn't need to be sorted.
+ */
+
+template <typename T>
+auto std_find(const std::vector<T>& vec, T key) -> std::optional<int> {
+    auto it = std::find(vec.begin(), vec.end(), key);
+    if (it == vec.end()) {
+        return std::nullopt;
+    }
+    return std::distance(vec.begin(), it);
+}
--- src/algorithms/search_array.cpp
+++ src/algorithms/search_array.cpp
@@ -10,58 +10,15 @@
  *
  */
 
+#include "search_array.h"
 #include <algorithm>
 #include <iostream>
-#include <numeric>
-#include <optional>
 #include <vector>
 #include "array_utils.h"
 
-/* Classic Binary Search
- * Time Complexity - O(log n), Space Complexity - O(1)
- * Note: Array should be sorted
- */
-
-template <typename T>
-auto binary_search(const std::vector<T>& vec, T key) -> std::optional<int> {
-    int size = vec.size();
-    int low = 0;
-    int high = size - 1;
-    while (low <= high) {
-        int mid = (high + low) / 2;
-        if (vec[mid] == key) {
-            return mid;
-        }
-        if (vec[mid] < key) {
-            low = mid + 1;
-        } else {
-            high = mid - 1;
-        }
-    }
-    return std::nullopt;
-}
-/* Cache Efficient Binary Search
- * Note: Array should be sorted
- */
-template <typename T>
-auto binary_jump_search(const std::vector<T>& vec, T key) -> std::optional<int> {
-    int size = vec.size();
-
-    int index = 0;
-    for (int jump = size / 2; jump >= 1; jump /= 2) {
-        while (index + jump < size && vec[index + jump] <= key) {
-            index += jump;
-        }
-    }
-    if (vec[index] == key) {
-        return index;
-    }
-    return std::nullopt;
-}
 auto main() -> int {
     // Create a vector of 10 elements
     auto vec = random_vector<int>(20);
-    // Print the vector
     std::sort(vec.begin(), vec.end());
     printVec<int>(vec);
     int input = 0;
@@ -72,7 +29,8 @@ auto main() -> int {
             break;
         }
         // auto index = binary_search(vec, input);
-        auto index = binary_jump_search(vec, input);
+        // auto index = binary_jump_search(vec, input);
+        auto index = std_find(vec, input);
         if (index.has_value()) {
             std::cout << "Element " << input << " found at index: " << index.value() << "\n";
         } else {
