0dbf80fe6391e9986fee72abb4c7debeda716c6f
linem davton <linem.davton@gmail.com>
2025-02-10 16:04:43 +0100

    complete_search: subset enumeration

---
+++ src/algorithms/complete_search.cpp
@@ -0,0 +1,36 @@
+#include <cstddef>
+#include <vector>
+
+#include "array_utils.h"
+template <typename T>
+/*
+ * @breif
+ * @detail Creates a binary decision tree, each element can either be in or out of the subset
+ * First Branch: Consider the case where current index element is not part of the subset
+ * Second Branch: Make the current element part of the subset.
+ * Cleanup: remove the element from the subset vector.
+ */
+auto generateSubsets(std::vector<T>& vec, std::size_t index, std::vector<T>& subset) {
+    // Base Case: when each element has been considered, index points past the last element
+    if (index == vec.size()) {
+        printVec(subset);
+        return;
+    }
+
+    // Case 1: Exclude the current element being considered
+    generateSubsets(vec, index + 1, subset);
+
+    // Case2: Include the current element being considered
+    subset.push_back(vec[index]);
+    generateSubsets(vec, index + 1, subset);
+    // Backtrack, return to previosu state, remove the element
+    subset.pop_back();
+    return;
+}
+
+auto main() -> int {
+    std::vector<int> vec = {1, 2, 3};
+    std::vector<int> subset;
+    generateSubsets(vec, 0, subset);
+    return 0;
+}
