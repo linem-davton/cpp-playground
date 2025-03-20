49f76a85c77d4ea553e964fd3e286fee1c16fb87
linem davton <linem.davton@gmail.com>
2025-03-20 11:00:41 +0100

    problems:array

--- src/problems/array_easy.cpp
+++ src/problems/array_easy.cpp
@@ -1,11 +1,11 @@
 #include <algorithm>
 #include <cstddef>
 #include <iostream>
+#include <map>
 #include <set>
 #include <unordered_set>
 #include <vector>
 #include "utils.h"
-
 /*
  * Given an array of integers, find if the array contains any duplicates.
  * Solutions:
@@ -55,10 +55,42 @@ auto twoSum(std::vector<int>& nums, int target) -> std::vector<int> {
     return {};
 }
 
+/* Given a non-empty array of integers, return the k most frequent elements.
+ * Solutions:
+ * 1. Use a map to store the frequency of each element.
+ * 2. Sort the map by frequency and return the first k elements.
+ */
+auto topKFrequent(std::vector<int>& nums, int k) -> std::vector<int> {
+    // Make a frequency table for array, best way is to use sorted map
+
+    std::map<int, int> frq;
+    std::vector<std::pair<int, int>> out;
+    for (const auto& e : nums) {
+        frq[e]++;
+    }
+
+    // make a vec of tuple with
+    for (const auto& pair : frq) {
+        out.emplace_back(pair.second, pair.first);
+    }
+
+    // Return the first k
+    std::sort(out.begin(), out.end());
+    std::vector<int> res(k);
+    for (int i = 0; i < k; i++) {
+        res[i] = out[out.size() - i - 1].second;
+    }
+
+    return res;
+}
+
 auto main() -> int {
-    std::vector<int> v{-1, -2, -3, -4, -5};
+    std::vector<int> v{-1, -2, -3, -4, -5, -1, -5, -5};
     printVec(v);
     std::cout << "Duplicates found in array: " << hasDuplicate(v) << std::endl;
     auto res = twoSum(v, -8);
     printVec(res);
+
+    auto res2 = topKFrequent(v, 2);
+    printVec(res2);
 }
